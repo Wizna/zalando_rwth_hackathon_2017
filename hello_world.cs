84f125eeaed5a33bf8912bec5433dddda4850de9
Ajay Mandyam Rangarajan <ajay.rangarajan@rwth-aachen.de>
2017-05-12 18:14:11 +0200

    Hello world trial

---
+++ hello_world.cs
@@ -0,0 +1,10 @@
+// HelloWorld.cs    (outputs a line of text to the console)
+using UnityEngine;
+public class HelloWorld : MonoBehaviour {
+
+
+ void Start() {
+     Debug.Log("Hello World!");
+ }
+
+}
